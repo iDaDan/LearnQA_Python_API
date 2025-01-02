417edf190607435d825346832762e1e7f2ac556f
idadan <d9093211188@gmail.com>
2025-01-03 00:03:35 +0300

    Реализовано: выводим количество редиректов и итоговый URL

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import requests
 
 response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
 amount = len(response.history)
-last_url = response.history[amount-1].url
+last_url = response.history[amount-2].url
 
 print("Редиректов:"+str(amount)+", последний урл: "+last_url)
