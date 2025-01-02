fc14f97d43b7e164bc52568b126ebff8dfea43e4
idadan <d9093211188@gmail.com>
2025-01-02 23:12:25 +0300

    Выводим все URL

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
+def print_all_url():
+    response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
+    #amount = response.history
+    #first_resp = response.history[0]
+    #sec_resp = response.history[1]
+    # print(first_resp.url)
+    # print(sec_resp.url)
+    u = 0
+    while u < (len(response.history)):
+        print(response.history[u].url)
+        u +=1
+
+
+print_all_url()
