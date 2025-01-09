dc45228cfff28f7acd7ee9d0c79174e4375c3360
idadan <d9093211188@gmail.com>
2025-01-09 15:50:48 +0300

    Цикл для методов кроме get

--- main.py
+++ main.py
@@ -1,43 +1,71 @@
+from gettext import gettext
+
 import requests
 
+from json_parsing import obj
 
 response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
 amount = len(response.history)
-last_url = response.history[amount-2].url
-
-#print("Редиректов:"+str(amount)+", последний урл: "+last_url)
+last_url = response.history[amount - 2].url
 
 
-#Скрипт для ex7: запросы и методы
+# print("Редиректов:"+str(amount)+", последний урл: "+last_url)
+
+
+# Скрипт для ex7: запросы и методы
 def types_and_methods_test():
     response_get_no_papam = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type")
     print(response_get_no_papam)
-    print(response_get_no_papam.url)
+    print("post, no data")
 
     response_get_no_param_head_test = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type")
     print(response_get_no_param_head_test)
-    print(response_get_no_param_head_test.url)
+    print("head, no data")
 
-    response_get_type_post_test = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data="POST")
+    response_get_type_post_test = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type",
+                                                data="POST")
     print(response_get_type_post_test)
-    print(response_get_type_post_test.url)
+    print("post, data post")
+
+    response_get_type_post_test = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type",
+                                                data="DELETE")
+    print(response_get_type_post_test)
+    print("post, data Delete")
+
+types_and_methods_test()
 
 def all_types_and_methods_test():
     method_type = ["get", "post", "put", "delete"]
     data = ["GET", "POST", "PUT", "DELETE"]
-    type_number = 0
-    data_number = 0
-    hell_and_heaven_epic_divider = 0
 
-    while type_number<4:
-        type = method_type[type_number]
+    data_type_number = 1
+    # рабочая конкретная строка:
+    #print(requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data="POST"))
+    #method_name in method_type:
+    method_type = ["get", "post", "put", "delete"]
+    method_type_number = 1
+    while method_type_number < 4:
+        method = getattr(requests, method_type[method_type_number])
+        data = ["GET", "POST", "PUT", "DELETE"]
+        data_type_number = 1
+        while data_type_number < 4:
+            print("method:", method_type[method_type_number]," data:", data[data_type_number], )
+            print(method("https://playground.learnqa.ru/ajax/api/compare_query_type",
+                         data=str(data[data_type_number])))
+            print((method("https://playground.learnqa.ru/ajax/api/compare_query_type",
+                         data=str(data[data_type_number]))).url)
+            data_type_number += 1
+        method_type_number += 1
+
+
+all_types_and_methods_test()
+"""
         if type_number !=0:
             data_number = 0
             print(requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data="POST"))
         else:
             response_get_type_post_test = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type",
                                                         data="POST")
-
-
-
-types_and_methods_test()
+"""
+# def post_type_methods_tests():
+# types_and_methods_test()
