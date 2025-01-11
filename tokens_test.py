f8494d6309e56ea8de6dad1f729587a68ebcba3b
idadan <d9093211188@gmail.com>
2025-01-11 20:57:58 +0300

    Скелетик задания 8

--- tokens_test.py
+++ tokens_test.py
@@ -1,7 +1,13 @@
 import requests
 
 def tokens_test():
+    # создаём кипучую задачу
     resp=requests.get("https://playground.learnqa.ru/ajax/api/longtime_job")
     print(resp.text)
+    # теребим задачу пока не готова, для этого подставляем кипучий токен, проверяем что status Job is NOT ready
+    resp_not_ready_status=requests.get("https://playground.learnqa.ru/ajax/api/longtime_job")
+    # усыпляем на количество секунд из первого метода, проверяем что status Job is NOT ready
+    resp_result_status = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job")
+
 
 tokens_test()
