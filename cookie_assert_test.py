ace48201fd214dae03a946d3c9d2744a99eec6a4
idadan <d9093211188@gmail.com>
2025-01-18 23:36:00 +0300

    Реализована проверка валидности куки

---
+++ cookie_assert_test.py
@@ -0,0 +1,11 @@
+import pytest
+import requests
+import json
+
+class TestCookieValue:
+    def test_cookie_value(self):
+        value = "<RequestsCookieJar[<Cookie HomeWork=hw_value for .playground.learnqb.ru/>]>"
+        response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
+        resp_cookie= response.cookies
+        print(resp_cookie)
+        assert str(resp_cookie) == value, "кука приболела (cookie invalid)"
