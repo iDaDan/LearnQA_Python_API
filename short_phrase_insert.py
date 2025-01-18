993546a0fd51b59d5d95cbdb3e6be5c92292f1c1
idadan <d9093211188@gmail.com>
2025-01-18 21:30:01 +0300

    Реализована проверка длины вводимого слова

--- short_phrase_insert.py
+++ short_phrase_insert.py
@@ -1,8 +1,11 @@
-from itertools import count
-
 import pytest
 
-phrase = input("Set a phrase: ")
-j = str(len(phrase))
-if j < 15
-print(j)
+class TestPhraseLength:
+    def test_phrase_len(self):
+        phrase = input("Set a phrase: ")
+        j = len(phrase)
+
+        assert j<15, "phrase is more then 15 characters"
+        # if j < 15:
+        #     print(f"phrase lenh")
+        # else:
