9d23812fc23d1704c1fdfc73fec8db1a7fb109ec
idadan <d9093211188@gmail.com>
2025-02-11 13:50:50 +0300

    Реализован ассерт хедеров

--- test_assert_headers.py
+++ test_assert_headers.py
@@ -1,7 +1,20 @@
 import requests
 
+
 def test_assert_headers():
+    expected_headers_values = {'Date': 'Tue, 11 Feb 2025 09:30:36 GMT', 'Content-Type': 'application/json',
+                               'Content-Length': '15',
+                               'Connection': 'keep-alive', 'Keep-Alive': 'timeout=10', 'Server': 'Apache',
+                               'x-secret-homework-header': 'Some secret value', 'Cache-Control': 'max-age=0',
+                               'Expires': 'Tue, 11 Feb 2025 09:30:36 GMT'}
+
     response = requests.get("https://playground.learnqa.ru/api/homework_header")
-    headers = response.headers
-    response_as_dict = response.json()
-    print(f"{headers}, {response_as_dict}")
+
+    #print(f"\n HEADERS: {headers}")
+    response_headers = response.headers
+
+    for key, value in expected_headers_values.items():
+
+        assert key in response_headers, f"no {key} in response_headers"
+        assert response_headers.get(key) == value, f"Значение для {key} не совпадает. Ожидаем {value}, получаем {response_headers.get(key)}"
+
