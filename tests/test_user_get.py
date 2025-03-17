6b60128a8be2808f21115e459f7182597429d1b8
idadan <d9093211188@gmail.com>
2025-03-17 16:54:34 +0300

    Ex 16. test_get_user_details_auth_as_another_user

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -1,3 +1,5 @@
+import json
+
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 from lib.my_requests import MyRequests
@@ -30,3 +32,25 @@ class TestUserGet(BaseCase):
 
         expected_fields = ["username","email", "firstName", "lastName"]
         Assertions.assert_json_has_keys(response2, expected_fields)
+
+    def test_get_user_details_auth_as_another_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response1 = MyRequests.post("/user/login", data=data)
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+
+        response2 = MyRequests.get(f"/user/3",
+                                 headers={"x-csrf-token": token},
+                                 cookies= {"auth_sid": auth_sid})
+
+
+        Assertions.assert_json_has_key(response2,"username")
+        unexpected_keys = {}
+        if len(response2.json()) > 1:
+            for key in response2:
+                unexpected_keys[key] = [key]
+            assert False, f"response has unexpected key(s) {unexpected_keys}"
